ae2dd3c1691167a799a4bb16d4a15ccd146d5f0b
luke30001 <41565281+luke30001@users.noreply.github.com>
2023-01-26 18:35:54 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
-wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
+wget https://github.com/luke30001/nasluke/raw/main/ngrok-v3-stable-linux-amd64.tgz
 unzip ngrok.zip
 ngrok config add-authtoken 3maBhEwKZgiZ3zDsdDbq3_jiAWnrrCZCzJHWrJR6im
