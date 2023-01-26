840cea55b7ddd0a1cd4d6c470860174c90ad4059
luke30001 <41565281+luke30001@users.noreply.github.com>
2023-01-26 18:37:51 +0100

    Update and rename test2.sh to test3.sh

--- test2.sh
+++
@@ -1,3 +0,0 @@
-wget https://github.com/luke30001/nasluke/raw/main/ngrok-v3-stable-linux-amd64.tgz
-unzip ngrok.zip
-ngrok config add-authtoken 3maBhEwKZgiZ3zDsdDbq3_jiAWnrrCZCzJHWrJR6im
---
+++ test3.sh
@@ -0,0 +1,3 @@
+wget https://raw.githubusercontent.com/luke30001/nasluke/main/ngrok-v3-stable-linux-amd64.tgz
+unzip ngrok.zip
+ngrok config add-authtoken 3maBhEwKZgiZ3zDsdDbq3_jiAWnrrCZCzJHWrJR6im
